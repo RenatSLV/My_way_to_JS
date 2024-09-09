9a0403dca297e586e14009ff89f12241ba43373c
Renat <renat.rakimzhanov@mail.ru>
2024-09-09 20:55:44 +0500

    introduction in js

---
+++ introduction_in_JS_1/script.js
@@ -0,0 +1,49 @@
+// task1
+// let $name$ = prompt("Введите ваше имя:");
+// console.log(`Привет ${$name$}!!`);
+
+// task2
+// let birth = +prompt("Введите год вашего рождения:");
+// const year = 2024;
+// console.log(year - birth);
+
+// task3
+// let P = +prompt("Введите длину стороны квадрата:");
+// console.log(`Периметр квадрата равен: ${P * 4}`);
+
+// task4
+// let R = +prompt("Введите радиус окружности:");
+// const Pi = 3.14;
+// let S = Pi * R ** 2;
+// console.log(`Площадь окружности: ${S}`);
+
+// task5
+// let cross = +prompt("Введите расстояние в км между двумя городами:");
+// let time = +prompt("Введите за сколько часов вы хотите добраться:");
+// console.log(`Рекомендуемая скорость ${cross / time}Km/h`);
+
+// task6
+// let $Dolars = +prompt("Введите сумму в долларах:");
+// const Evra = 0.92;
+// let sum = $Dolars * Evra / 1;
+// console.log(`${$Dolars}$ = ${sum}\u20ac`);
+
+// task7
+// let Gig = +prompt("Введите обЪём флэшки:");
+// let Gig_in_Meg = Gig * 1024;
+// console.log(`На флешку помещается файлов размером в 820 Мб: ${Math.floor(Gig_in_Meg / 820)}`);
+
+// task8
+// let Sum_money = +prompt("Введите сколько у вас Тенге в кошельке:");
+// let Price_chocolat = +prompt("Введите цену одной шоколадки в Тенге:");
+// console.log(`Вы можете купить на ${Sum_money}\u20B8 столько шоколадка ${Math.floor(Sum_money / Price_chocolat)}шт. И у вас останется ${Math.floor(Sum_money % Price_chocolat)}\u20B8`);
+
+// task9
+// let numbers = +prompt("Введите трёх значное чсило:");
+// let start = numbers % 10;
+// let midle = Math.floor((numbers % 100) / 10);
+// let end = Math.floor(numbers / 100);
+// let $string1 = String(start); 
+// let $string2 = String(midle); 
+// let $string3 = String(end); 
+// console.log(`${numbers} = ${$string1 + $string2 + $string3}`);
